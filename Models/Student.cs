af7c4039dd49c139e20daa130506da0cd72892b5
Kyth Carael <kythcarael04@gmail.com>
2025-09-29 07:58:47 +0800

    Database not yet connected

---
+++ Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations; 
+
+namespace MyWebApplication.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required,MaxLength(25)]
+        public string? LastName { get; set; }
+
+        [Required, MaxLength(25)]
+        public string? FirstName { get; set; }
+        public int? Age { get; set; }
+        public string? Course { get; set; }
+        public string Email { get; set; } = "";
+
+        [Display(Name = "Date Created")]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+    }
+}
